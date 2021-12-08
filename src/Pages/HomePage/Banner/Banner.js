d3309a947f6d19415bd4f20834132246a31bbe04
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2021-12-08 18:44:03 +0600

    banner done

--- src/Pages/HomePage/Banner/Banner.js
+++ src/Pages/HomePage/Banner/Banner.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import frame from '../../../images/logos/Frame.png';
 
 const Banner = () => {
     return (
-        <div>
-
+        <div className='lg:flex p-5 justify-center align-center bg-custom'>
+            <div className='lg:pr-24 lg:gap-4 text-left flex flex-col justify-center align-center'>
+                <h1 className='text-4xl lg:text-6xl font-bold'>Let’s Grow Your Brand To The Next Level</h1>
+                <p>
+                    Having a great product available to your customers at a great price does absolutely nothing for you if your customers don’t know about it.
+                </p>
+                <NavLink className="inline-block w-1/4 text-center text-sm px-4 py-4 login-btn leading-none border rounded text-white border-white hover:border-black hover:text-black hover:bg-white mt-4 lg:mt-0" to="/hire">Hire Us</NavLink>
+            </div>
+            <div>
+                <img src={frame} alt="" />
+            </div>
         </div>
     );
 };
--- src/Pages/HomePage/Navbar/Navbar.js
+++ src/Pages/HomePage/Navbar/Navbar.js
@@ -15,8 +15,8 @@ const Navbar = () => {
                     <img className='w-32' src={logo} alt="Creative agency logo" />
                 </div>
                 <div className="block lg:hidden mt-3">
-                    <button onClick={menuToggleHandler} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                    <button onClick={menuToggleHandler} className="flex items-center px-3 py-2 border rounded border-black-400 hover:text-white hover:border-white">
+                        <svg className="fill-current text-black h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                     </button>
                 </div>
                 <div className={`w-screen ${isOpen} flex-grow lg:flex text-right lg:items-center lg:w-auto`}>
