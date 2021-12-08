059a18e13f0c5263cb78c31088917f024ab7e8ef
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2021-12-08 18:15:08 +0600

    navigation bar complete

---
+++ src/Pages/HomePage/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import logo from '../../../images/logos/logo.png';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState('hidden');
+    const menuToggleHandler = () => {
+        setIsOpen(isOpen === 'hidden' ? 'block' : 'hidden');
+    }
+
+    return (
+        <div>
+            <nav className="flex items-center justify-between flex-wrap bg-custom p-6">
+                <div className="block">
+                    <img className='w-32' src={logo} alt="Creative agency logo" />
+                </div>
+                <div className="block lg:hidden mt-3">
+                    <button onClick={menuToggleHandler} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                        <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+                    </button>
+                </div>
+                <div className={`w-screen ${isOpen} flex-grow lg:flex text-right lg:items-center lg:w-auto`}>
+                    <div className="text-sm lg:flex-grow">
+                        <NavLink className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4" to="/">Home</NavLink>
+                        <NavLink className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4" to="#our-portfolio">Our Portfolio</NavLink>
+                        <NavLink className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4" to="#our-team">Our Team</NavLink>
+                        <NavLink className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-white mr-4" to="#contact">Contact</NavLink>
+                        <NavLink className="inline-block text-sm px-4 py-2 login-btn leading-none border rounded text-white border-white hover:border-black hover:text-black hover:bg-white mt-4 lg:mt-0" to="/login">Login</NavLink>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
