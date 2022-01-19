16a3ffad86d4240aced1701bac826329cd2bbf5d
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2022-01-19 14:51:42 +0600

    our works ui fixed

--- src/Pages/HomePage/OurWorks/OurWorks.js
+++ src/Pages/HomePage/OurWorks/OurWorks.js
@@ -6,11 +6,11 @@ const OurWorks = () => {
             <h1 className='text-3xl text-white font-bold'>Here Are Some of <span className='text-green-500'>Our Works</span></h1>
             <div>
                 <div className='grid-cols-3 gap-x-6 sm:block md:block lg:grid xl:grid p-8'>
-                    <img className='scale-image' src="https://i.ibb.co/1z6HVHN/carousel-1.png" alt="carousel-1" border="0" />
-                    <img className='scale-image' src="https://i.ibb.co/5Bkkvf5/carousel-2.png" alt="carousel-2" border="0" />
-                    <img className='scale-image' src="https://i.ibb.co/mHxbQ5m/carousel-3.png" alt="carousel-3" border="0" />
-                    <img className='scale-image' src="https://i.ibb.co/PZSW3Mx/carousel-4.png" alt="carousel-4" border="0" />
-                    <img className='scale-image' src="https://i.ibb.co/ggpSsX6/carousel-5.png" alt="carousel-5" border="0" />
+                    <img className='scale-image mt-2' src="https://i.ibb.co/1z6HVHN/carousel-1.png" alt="carousel-1" border="0" />
+                    <img className='scale-image mt-2' src="https://i.ibb.co/5Bkkvf5/carousel-2.png" alt="carousel-2" border="0" />
+                    <img className='scale-image mt-2' src="https://i.ibb.co/mHxbQ5m/carousel-3.png" alt="carousel-3" border="0" />
+                    <img className='scale-image mt-2' src="https://i.ibb.co/PZSW3Mx/carousel-4.png" alt="carousel-4" border="0" />
+                    <img className='scale-image mt-2' src="https://i.ibb.co/ggpSsX6/carousel-5.png" alt="carousel-5" border="0" />
                 </div>
             </div>
         </div>
